d4898c678c951ed20409f1ae0a6446326aaa36f1
Diego Martinez <diegofernandomartinezcalderon@gmail.com>
2023-07-09 17:25:46 -0500

    feat: servidor y prueba de api

---
+++ app.js
@@ -0,0 +1,21 @@
+import {createServer} from "http";
+import fs from "fs";
+const appHttp =createServer((req,res)=>{
+  if(req.url == "/"){
+   fs.readFile("index.html", "utf8", (err, data)=>{
+    res.end(data);
+   });
+  }else if(req.url == "/home"){
+    fs.readFile("prueba.html", "utf8", (err, data)=>{
+      res.end(data);
+    })
+   }
+});
+const config = {
+  hostname: "127.1.1.1",
+  port: 5000
+};
+appHttp.listen(config,()=>{
+    console.log(`http://${config.hostname}:${config.port}`);
+})
+
