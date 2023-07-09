66206637af85f94e719efd685df47d4bb541349d
Diego Martinez <diegofernandomartinezcalderon@gmail.com>
2023-07-09 17:28:27 -0500

    feat: :coffin: remover archivo de prueba

--- api.js
+++
@@ -1,9 +0,0 @@
-alert("hola");
-
-
-// fetch('http://api.nasa.gov/neo/rest/v1/neo/3542519?api_key=3adwX615fZdIHroQt8AmDhhfUg3w2laMekjjRdII');
-// then(response => response.json())
-//   .then(data => { 
-// console.log(data);
-//   });
-
